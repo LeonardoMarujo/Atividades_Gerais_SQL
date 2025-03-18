d31ed232117ebd3e5a7cd2db4234dd3b6ae1f869
Leonardo Marujo <leo.m.araujo12@gmail.com>
2025-03-18 14:13:33 -0300

    Delete SQL directory

--- SQL/atividades_gerais_p1.sql
+++
@@ -1,8 +0,0 @@
--- Quantos produtos temos na categoria 'artes'?
-
-SELECT
-    product_category_name as categorias,
-    count(DISTINCT(product_id)) as quantidade
-FROM tb_products
-
-WHERE product_category_name = 'artes'
